30dbb436bd0c1f04ba0f09083edbf501cf6126ca
Lional Darwin Arokiaraj <lional.arokiaraj@forsys.taas.com>
2022-10-04 12:24:59 +0530

    code update

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,22 @@
-import { AppComponent } from "./app.component"
+import { of } from "rxjs";
+import { AppComponent } from "./app.component";
+//import { HttpClient } from "@angular/common/http";
+
+// describe('AppComponent', () => {
+//   let fixture: AppComponent;
+//   //let httpClient: HttpClient;
+//   let httpClientSpy: any;
+
+//   beforeEach(() => {
+//     httpClientSpy = {
+//       get: jest.fn()
+//     }
+//     fixture = new AppComponent(httpClientSpy);
+//   });
+
+
+
+// });
 
 describe('AppComponent', () => {
   let fixture: AppComponent;
@@ -7,8 +25,32 @@ describe('AppComponent', () => {
     fixture = new AppComponent();
   });
 
-  it('test title', () => {
-    expect(fixture.title).toEqual('angular-jest');
+  it('test title',()=>{
+    expect(fixture.title).toBe('angular-jest');
   });
 
-})
+  it('test number',()=>{
+    expect(fixture.number).toBe(1);
+  });
+
+  it('test string',()=>{
+    expect(fixture.string).toBe('1');
+  });
+
+  it('test boolean',()=>{
+    expect(fixture.boolean).toBe(true);
+  });
+
+  it('test array',()=>{
+    expect(fixture.array).toEqual([1, 2, 3, 4, 5]);
+  });
+
+  it('test object',()=>{
+    expect(fixture.object).toEqual({ a: 1, b: 2, c: 3 });
+  });
+
+  it.only('test method-sum',()=>{
+    expect(fixture.sum(1,2)).toBe(3);
+  });
+
+});
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,41 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-jest';
+
+  number = 4;
+  string = '1';
+  boolean = true;
+
+  array = [1, 2, 3, 4, 5];
+  object = { a: 1, b: 2, c: 3 };
+
+
+  constructor() {
+    this.sum(this.number, this.number,);
+  }
+
+  sum(a: number, b: number): number {
+    return a+b;
+  }
+  
+  sum1(a: number, b: number): number {
+    return a+b;
+  }
+  sum2(a: number, b: number): number {
+    return a+b;
+  }
+  sum3(a: number, b: number): number {
+    return a+b;
+  }
+  sum4(a: number, b: number): number {
+    return a+b;
+  }
+  sum6(a: number, b: number): number {
+    return a+b;
+  }
+  sum5(a: number, b: number): number {
+    return a+b;
+  }
+
+
 }
